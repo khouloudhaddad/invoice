f904ed0d3822ecb706fbdcae5c48a4af234d2b8b
khouloudhaddad <khouloud.haddad@gmail.com>
2023-05-10 20:39:32 +0100

    Add vue to Laravel project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import { createApp } from "vue";
+
+import app from './components/app.vue'
+
+createApp(app).mount('#app')
---
+++ resources/js/components/app.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Hello from vue !!</h1>
+</template>
