44349397c93a3ea90422a12b7b71cd9cbf125bdd
khouloudhaddad <khouloud.haddad@gmail.com>
2023-05-10 19:59:33 +0100

    Models, factories & DB Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+       //  \App\Models\Counter::factory(1)->create();
+       //  \App\Models\Product::factory(5)->create();
+       //\App\Models\Customer::factory(5)->create();
+//        \App\Models\Invoice::factory(5)->create();
+//       \App\Models\InvoiceItem::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
