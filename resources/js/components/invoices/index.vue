bd4b9a61894b079e33aab8b608dd633ed41df4a6
khouloudhaddad <khouloud.haddad@gmail.com>
2023-05-11 01:03:45 +0100

    Invoices List

--- resources/js/components/invoices/index.vue
+++ resources/js/components/invoices/index.vue
@@ -1,9 +1,103 @@
 <script setup>
+    import {ref, onMounted} from "vue";
+
+    let invoicesList = ref([]);
+
+    onMounted(async ()=> {
+        console.log("here")
+        getInvoices();
+    });
+
+
+    const getInvoices = async () => {
+        await axios.get("/api/get_invoices_list").then((response)=>{
+
+            invoicesList.value = response.data.invoices;
+        })
+
+    }
+
 
 </script>
 
 <template>
-    <h1>Indes Invoice page</h1>
+    <div class="container">
+
+        <!--==================== INVOICE LIST ====================-->
+        <div class="invoices">
+
+            <div class="card__header">
+                <div>
+                    <h2 class="invoice__title">Invoices</h2>
+                </div>
+                <div>
+                    <a class="btn btn-secondary">
+                        New Invoice
+                    </a>
+                </div>
+            </div>
+
+            <div class="table card__content">
+                <div class="table--filter">
+                <span class="table--filter--collapseBtn ">
+                    <i class="fas fa-ellipsis-h"></i>
+                </span>
+                    <div class="table--filter--listWrapper">
+                        <ul class="table--filter--list">
+                            <li>
+                                <p class="table--filter--link table--filter--link--active">
+                                    All
+                                </p>
+                            </li>
+                            <li>
+                                <p class="table--filter--link ">
+                                    Paid
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="table--search">
+                    <div class="table--search--wrapper">
+                        <select class="table--search--select" name="" id="">
+                            <option value="">Filter</option>
+                        </select>
+                    </div>
+                    <div class="relative">
+                        <i class="table--search--input--icon fas fa-search "></i>
+                        <input class="table--search--input" type="text" placeholder="Search invoice">
+                    </div>
+                </div>
+
+                <div class="table--heading">
+                    <p>ID</p>
+                    <p>Date</p>
+                    <p>Number</p>
+                    <p>Customer</p>
+                    <p>Due Date</p>
+                    <p>Total</p>
+                </div>
+
+                <!-- item 1 -->
+                <div class="table--items" v-for="invoice in invoicesList" :key="invoice.id" v-if="invoicesList && invoicesList.length > 0">
+                    <a href="#" class="table--items--transactionId">#{{ invoice.id }}</a>
+                    <p>{{ invoice.date }}</p>
+                    <p>#{{ invoice.number }}</p>
+                    <p v-if="invoice.customer">{{ invoice.customer.firstname }}</p>
+                    <p v-else></p>
+                    <p>{{ invoice.due_date }}</p>
+                    <p> $ {{ invoice.total }}</p>
+                </div>
+
+                <div class="table--items text-center" v-else>
+                    <p>No invoices found</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
 </template>
 
 <style scoped>
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\InvoiceController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/get_invoices_list", [InvoiceController::class, 'getInvoices']);
